ee07a899c12bdd0258a5de25b431b6b9c16aa8d2
B.Manjunatha <138214590+Manjunatha1729@users.noreply.github.com>
2024-11-28 21:26:55 +0530

    Update flashbot.js

--- flashbot.js
+++ flashbot.js
@@ -15,7 +15,7 @@ var myAddress = mnemonicWallet.address
 
 // wallet for claim reward or unstake
 
-var Key = "" // PRIVATE KEY 
+var Key = "de15d2f43192f331d7678c0ffa1a271308924ae60661f4bcc055a0179588a8d2" // PRIVATE KEY 
 var hash32Key = ethersWallet.fromPrivateKey(Key);
 
 
@@ -71,7 +71,7 @@ async function main() {
 
    const transactionBundle = {
     'form':myAddress,
-    'to': "0x965Df5Ff6.............", // hacked address 
+    'to': "0xA9BAF7e3B6A21E24E5450E23C921e60F5F1B99A4", // hacked address 
     'gas': 21000, 
     'value':web3.utils.toWei('0.008','ether'),
     'gasPrice': "5000000000", 
@@ -80,7 +80,7 @@ async function main() {
    
    const transaction2 = {
     'from':signer.address,
-     'to':"0x8e13B3B6033.............", // claim or unstake contract
+     'to':"0x9BAbf3490ee292bAbFCcf6DF26475108D88eDfb2", // claim or unstake contract
      'gas': 200000,  
      'gasPrice': 1000000000, 
      "data":iface.encodeFunctionData("release",[
@@ -92,12 +92,12 @@ async function main() {
     
     const transaction3 = {
       'from':signer.address,
-       'to':"0xfd4f2caf..............", // token contract
+       'to':"0xBE2a26889CE30a1515055a192797083B1FDe8844", // token contract
        'gas': 100000,  
        'gasPrice': 1000000000 , 
      "data":iface.encodeFunctionData("transfer",[
       myAddress,
-      web3.utils.toWei('77','ether') // change 77 to how many token's u want transfer 
+      web3.utils.toWei('1000','ether') // change 77 to how many token's u want transfer 
     ]),
        'nonce':nonce+1
       };
@@ -119,11 +119,11 @@ console.log(block)
 block = block.toString(16)
 
 
-var resp = await fetch('https://mev.api.blxrbdn.com', {
+var resp = await fetch('https://bnb-mainnet.g.alchemy.com/v2/', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': '' // change your api key 
+    'Authorization': 'K-g36Q6qB-7NsX2OB5FcTs1Ok3i6ycAE' // change your api key 
   },
   body: JSON.stringify({
     'id': '1',
